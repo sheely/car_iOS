da2a79870e507e028b71cec562c4583f35d3e1af
zywang <zywang@isheely.com>
2014-09-22 08:58:08 +0800

    1

---
+++ Car/Car/Bussiness/Requirement/SHRequirementViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHRequirementViewController.h
+//  Car
+//
+//  Created by sheely.paean.Nightshade on 9/21/14.
+//  Copyright (c) 2014 sheely.paean.coretest. All rights reserved.
+//
+
+#import "SHViewController.h"
+
+@interface SHRequirementViewController : SHViewController
+
+@end
---
+++ Car/Car/Bussiness/Requirement/SHRequirementViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHRequirementViewController.m
+//  Car
+//
+//  Created by sheely.paean.Nightshade on 9/21/14.
+//  Copyright (c) 2014 sheely.paean.coretest. All rights reserved.
+//
+
+#import "SHRequirementViewController.h"
+
+@interface SHRequirementViewController ()
+
+@end
+
+@implementation SHRequirementViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
